73a5b15988c4070750e5ab7652c53ae790131628
Jordan West <jordwest@gmail.com>
2017-03-24 12:53:31 +1000

    Persists timeline even when disconnected

---
+++ client/state/happychat/constants.js
@@ -0,0 +1,3 @@
+export const HAPPYCHAT_CONNECTION_STATUS_CONNECTING = 'connecting';
+export const HAPPYCHAT_CONNECTION_STATUS_CONNECTED = 'connected';
+export const HAPPYCHAT_CONNECTION_STATUS_DISCONNECTED = 'disconnected';
